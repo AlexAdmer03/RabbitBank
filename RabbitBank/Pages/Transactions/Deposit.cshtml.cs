0ae6d699f6e028cef0e1361a49d45e40c79db257
AlexAdmer03 <alex.admer03@gmail.com>
2023-04-28 14:39:50 +0200

    New pages

---
+++ RabbitBank/Pages/Transactions/Deposit.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RabbitBank.Pages.Transactions
+{
+    public class DepositModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
