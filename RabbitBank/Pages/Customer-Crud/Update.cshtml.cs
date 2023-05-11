86331c3ec68f1330e7e7e7930eeccf3181840e8e
AlexAdmer03 <alex.admer03@gmail.com>
2023-05-11 12:36:47 +0200

    Created empty Crud

---
+++ RabbitBank/Pages/Customer-Crud/Update.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RabbitBank.Pages.Customer_Crud
+{
+    public class UpdateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
