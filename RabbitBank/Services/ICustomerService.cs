8202d6f63ad149f3244fa489a40ee42c0f634a8b
AlexAdmer03 <alex.admer03@gmail.com>
2023-05-23 09:32:28 +0200

    bug fix

--- RabbitBank/Services/ICustomerService.cs
+++ RabbitBank/Services/ICustomerService.cs
@@ -6,7 +6,6 @@ namespace RabbitBank.Services
     {
         List<Customer> GetCustomers(string searchTerm, string city, int pageNumber, int pageSize, string sortColumn, string sortOrder);
 
-        //Customer GetCustomerById(int customerId);
         int GetTotalCustomersCount(string searchTerm, string city);
 
     }
